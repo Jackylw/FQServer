92fc63409fe6849ea6f31cf65659ff6da472c732
Jackylw <1916450235@qq.com>
2024-03-07 14:23:56 +0800

    代码优化

--- src/main/java/top/fexample/fq/Application.java
+++ src/main/java/top/fexample/fq/Application.java
@@ -3,14 +3,18 @@ package top.fexample.fq;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import top.fexample.fq.model.ServerCommunication;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        ServerCommunication serverCommunication = new ServerCommunication();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/ServerView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setTitle("FQ服务器");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
